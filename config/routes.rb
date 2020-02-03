74a1b9c0b97d221b4764645145875635ecdac891
kamui58 <owlprotectthewillage.58@gmail.com>
2020-02-03 14:33:01 +0900

    devise_create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # , controllers: {
+  #   registrations: 'users/registrations',
+  #   sessions:      'users/sessions',
+  # }
   root to: "messages#index"
+  resources :users, only: [:index, :edit, :update]
+  resources :messages, only: [:index]
+
 end
