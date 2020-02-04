8fdaeb58d0b88c56e6295391076683cdc8209701
kamui58 <owlprotectthewillage.58@gmail.com>
2020-02-04 16:18:52 +0900

    mypage_update

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,8 +2,6 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
 
-    # @messages = Message
-    # .includes(:user).order("created_at DESC").page(params[:page]).per(5)
   end
 
   def create
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
-    
+    user = User.find(params[:id])
+    @nickname = user.nickname
+    @messages = user.messages
   end
 end
