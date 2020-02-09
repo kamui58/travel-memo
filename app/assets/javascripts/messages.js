852a9ccbcdb34d8633be651c9c0b8091d343f0d4
kamui58 <owlprotectthewillage.58@gmail.com>
2020-02-09 16:14:25 +0900

    form_update

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,15 +1,18 @@
 $(function(){
-  var elements = document.getElementsByClassName("drag-and-drop");
+  // var elements = document.getElementsByClassName("drag-and-drop");
+  var elements = document.getElementsByClassName("formBox");
+
 
   var x;
   var y;
 
   for(var i = 0; i < elements.length; i++) {
     elements[i].addEventListener("mousedown", mdown, false);
-    elements[i].addEventListener("touchstart", mdown, false);
+    // elements[i].addEventListener("touchstart", mdown, false);
   }
 
   function mdown(e){
+    console.log("押した")
     this.classList.add("drag");
 
     if(e.type === "mousedown"){
@@ -21,11 +24,14 @@ $(function(){
     x = event.pageX - this.offsetLeft;
     y = event.pageY - this.offsetTop;
 
+    // ここに"mousedown"から"mouseleave"離した時の処理を入れたい
+    // ここにif文を入れて、"mouseup"した場合の処理を入れればいいのでは？
     document.body.addEventListener("mousemove", mmove, false);
-    document.body.addEventListener("touchmove", mmove, false);
+    // document.body.addEventListener("touchmove", mmove, false);
   }
 
   function mmove(e) {
+    console.log("動かした")
     var drag = document.getElementsByClassName("drag")[0];
 
     if(e.type === "mousemove"){
@@ -41,17 +47,18 @@ $(function(){
 
     drag.addEventListener("mouseup", mup, false);
     document.body.addEventListener("mouseleave", mup, false);
-    drag.addEventListener("touchend", mup, false);
-    document.body.addEventListener("touchleave", mup, false);
+    // drag.addEventListener("touchend", mup, false);
+    // document.body.addEventListener("touchleave", mup, false);
   }
 
   function mup(e){
+    console.log("置いた")
     var drag = document.getElementsByClassName("drag")[0];
-    
+
     document.body.removeEventListener("mousemove", mmove, false);
-    drag.removeEventListener("mouseup", mup, false);
-    document.body.removeEventListener("touchmove", mmove, false);
-    drag.removeEventListener("touchend", mup, false);
+    drag.removeEventListener("mouseup", mup, false)
+    // document.body.removeEventListener("touchmove", mmove, false);
+    // drag.removeEventListener("touchend", mup, false);
 
     drag.classList.remove("drag");
   }
