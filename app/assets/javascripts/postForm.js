c6f6b93ac2a6926ecd603869ea6583d6c1489fb8
kamui58 <owlprotectthewillage.58@gmail.com>
2020-05-10 23:26:55 +0900

    コメントアウトの削除
    #What
    　必要ないコメントアウトを削除
    
    #Why
    　見やすくするため

--- app/assets/javascripts/postForm.js
+++ app/assets/javascripts/postForm.js
@@ -47,18 +47,10 @@ $(function(){
   }
 
   function mup(e){
-    // var drag = document.getElementsByClassName("drag")[0];
 
     document.body.removeEventListener("mousemove", mmove, false);
-    // console.log("1")
-    // drag.removeEventListener("mouseup", mup, false);
-    // console.log("2")
     document.body.removeEventListener("touchmove", mmove, false);
-    // console.log("3")
-    // drag.removeEventListener("touchend", mup, false);
-    // console.log("4")
 
-    // drag.classList.remove("drag");
   }
 
 });
