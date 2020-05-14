1c0091418ac2d8e85b6f83febe89e1ae5c86cd59
kamui58 <owlprotectthewillage.58@gmail.com>
2020-05-14 15:44:53 +0900

    GoogleMapsAPIの追加
    #What
    　観光地、施設名を確認した時に、左のリンクがGoogleMapに変更されるように修正した
    
    #Why
    　観光地、施設名の場所をわかりやすくするため

--- app/assets/javascripts/geocodingAPI.js
+++ app/assets/javascripts/geocodingAPI.js
@@ -10,6 +10,28 @@ function codeAddress() {
   geocoder.geocode({ 'address': inputAddress }, function (results, status) {
     if (status == 'OK') {
 
+      if (map != true) {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: results[0].geometry.location,
+          zoom: 9,
+          mapTypeControl: false,
+          streetViewControl: false,
+          draggable: false,
+          fullscreenControl: false,
+        });
+        marker = new google.maps.Marker({
+          position: map.getCenter(),
+          map: map,
+        });
+      } else {
+        marker.setMap(null);
+        map.setCenter(results[0].geometry.location);
+        marker = new google.maps.Marker({
+          position: results[0].geometru.location,
+          map: map,
+        })
+      }
+
       resultsAddress = results[0].formatted_address.replace(/日本、〒[/\d/]{3,}-[/\d/]{4,}/g, "");
       addressDis.innerHTML = resultsAddress;
 
