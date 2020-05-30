df11c3dbe8a8406f981584218dcd2221b899efab
kamui58 <owlprotectthewillage.58@gmail.com>
2020-05-30 11:57:27 +0900

    Revert "ハッシュタグ機能の追加"
    This reverts commit 32d36b58ec250575709daed68ec19445f8a09605.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "posts#index"
 
-  get "post/search"
-
   namespace :prefectures do
     resources :hokkaidos, only: :index
     resources :tohokus, only: :index
